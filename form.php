1d5cb081896832d3f12d672f8d0ecfea5b61343c
Angelica Agatha <125953295+angelicagatha@users.noreply.github.com>
2023-06-01 14:38:16 +0700

    form nya

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>Input Form</title>
+    </head>
+    <body>
+        <div class="form-input">
+            <form>
+                <input placeholder="Name" required="" type="text" /><input name="customerEmail" placeholder="Email" type="email" /><input name="customerPhone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone" type="tel" /><button type="button">SIGN UP</button>
+            </form>
+        </div>
+    </body>
+</html>
